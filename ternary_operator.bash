cdaead31dad609c31ea4ae36ddeedac1d76f5c9d
RawIron <manianology@gmail.com>
2025-07-21 12:42:23 -0700

    Explain ternary operator pitfall

--- ternary_operator.bash
+++ ternary_operator.bash
@@ -1,1 +1,25 @@
-[[ "$1" == "doit" ]] && echo "doing it" || echo "staying out of it"
+source bunit.bash
+
+
+# ternary operator
+
+# implement with && and ||
+#   this is not robust --> see pitfall
+ternary() {
+  local input="$1"
+  [[ "$input" == "doit" ]] && echo "doing it" || echo "staying out of it"
+}
+
+[[ $(ternary "doit") == "doing it" ]] ; should_pass
+[[ $(ternary "dont") == "staying out of it" ]] ; should_pass
+
+
+# the && A || B pitfall
+#   will execute B in case A failed
+ternary() {
+  local input="$1"
+  [[ "$input" == "doit" ]] && false || echo "staying out of it"
+}
+
+[[ $(ternary "doit") == "doing it" ]] ; should_fail
+[[ $(ternary "doit") == "staying out of it" ]] ; should_pass
