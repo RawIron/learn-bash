958baac8ccc1aeba918c4c5b140898cbaac2ab0f
RawIron <manianology@gmail.com>
2025-07-24 15:03:37 -0700

    Write 2 tests for compound assignment

--- dicts.bash
+++ dicts.bash
@@ -3,16 +3,26 @@ source bunit.bash
 
 
 # DICTIONARY
+#   associative array
 declare -A wordcount
 
 wordcount["bash"]=45
 wordcount["zsh"]=21
 [[ ${wordcount["zsh"]} -eq 21 ]] ; should_pass
 
+# compound assignment
+wordcount=("bash" 13 "zsh" 12)
+[[ ${wordcount["zsh"]} -eq 12 ]] ; should_pass
 
+wordcount=(["bash"]=3 \
+           ["zsh"]=2)
+[[ ${wordcount["zsh"]} -eq 2 ]] ; should_pass
+
+
+# count words in a text
 mapfile -t numbers< <(tail -n +2 | tr ' ' '\n')
-declare -A number_count
 
+declare -A number_count
 for number in ${numbers[@]}; do
     if [[ ${number_count[$number]+_} ]]; then
         number_count[$number]=1
