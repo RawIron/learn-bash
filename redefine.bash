9ba7a33325e256ebd1fc14e66785629c0783d3a2
RawIron <manianology@gmail.com>
2025-07-24 16:19:21 -0700

    Fix numeric variables do not require $ in arithmetic expressions

--- redefine.bash
+++ redefine.bash
@@ -1,7 +1,10 @@
 source ./bunit.bash
+#set -x
 
 
 # CONST VARIABLES
+#   readonly
+#   declare -r
 
 # a readonly variable
 readonly a=20
@@ -16,13 +19,22 @@ a=10
 # $a is unchanged
 [[ $a -eq 20 ]] ; should_pass
 
+# symbol _a_ can be used
+# to define a function
+a() {
+    echo "function a"
+}
+[[ $(a) == "function a" ]] ; should_pass
 
-# VARIABLES
+
+# NUMERIC VARIABLES
+#   declare -i
 
 # define symbol dinner
 # as integer variable
 declare -i dinner=12
 [[ $dinner -eq 12 ]] ; should_pass
+[[ dinner -eq 12 ]] ; should_pass
 
 # define symbol dinner
 # as function
@@ -43,23 +55,47 @@ dinner() {
 dinner=dinner
 [[ $dinner -eq 12 ]] ; should_pass
 
-# assign dinner to a new variable
+# assign dinner to a new variable _lunch_
+# the value of _lunch_ is a string
 lunch=dinner
+[[ $lunch == "dinner" ]] ; should_pass
+
+# recall from above that
+# with integer operators no _$_ prefix is required
+[[ dinner -eq 12 ]] ; should_pass
+# and so surprisingly
 [[ $lunch -eq 12 ]] ; should_pass
+# and at the same time
+[[ $lunch == "dinner" ]] ; should_pass
 
-# dinner points to both locations
-#
-# dinner ---> 14
-#        `--> lunch ---> 14
+# with integer operators
+# dinner and $lunch are the same
 dinner=14
-[[ $dinner -eq 14 ]] ; should_pass
-[[ $lunch  -eq 14 ]] ; should_pass
+[[ dinner -eq 14 ]] ; should_pass
+[[ $lunch -eq 14 ]] ; should_pass
+(( dinner + $lunch == 28 )) ; should_pass
+[[ ${lunch} -eq 14 ]] ; should_pass
+[[ "$lunch" -eq 14 ]] ; should_pass
+[[ "${lunch}" -eq 14 ]] ; should_pass
 
-# lunch points only to one location
-# lunch ---> 16
-lunch=16
-[[ $lunch  -eq 16 ]] ; should_pass
-[[ $dinner -eq 14 ]] ; should_pass
+# and still lunch contains the string "dinner"
+[[ $lunch == "dinner" ]] ; should_pass
+
+
+# if converting to an integer fails
+# the result is 0
+dinner="something"
+[[ $dinner -eq 0 ]] ; should_pass
+
+# make it an array of integers
+declare -a dinner=(4 7 11)
+[[ ${dinner[1]} -eq 7 ]] ; should_pass
+[[ ${dinner[@]} == "4 7 11" ]] ; should_pass
+
+# anything in the array that is not an integer
+# converts to 0
+dinner=(too 2 dinners)
+[[ ${dinner[@]} == "0 2 0" ]] ; should_pass
 
 
 # FUNCTIONS
