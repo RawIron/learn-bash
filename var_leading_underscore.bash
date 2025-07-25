40ecc61673257b8d00d90646e7598f37b2218189
RawIron <manianology@gmail.com>
2025-07-25 10:32:14 -0700

    Write 6 tests for leading underscore names

--- var_leading_underscore.bash
+++ var_leading_underscore.bash
@@ -1,12 +1,28 @@
-_name="zombie"
+source bunit.bash
 
-echo ${_name}
-echo $_name
+# a leading underscore is a valid name
+# it works for variables and functions
+
+_name="zombie"
+[[ ${_name} == "zombie" ]] ; should_pass
+[[ $_name == "zombie" ]] ; should_pass
+
+declare -n ref=_name
+[[ ${ref} == "zombie" ]] ; should_pass
 
 
 declare -A _dict
 _dict=(
-	["name"]="array_of_zombies"
+	["name"]="array of zombies"
 )
+[[ ${_dict["name"]} == "array of zombies" ]] ; should_pass
 
-echo ${_dict["name"]}
+
+_call_zombie() {
+  echo "hey zombie"
+}
+[[ $(_call_zombie) == "hey zombie" ]] ; should_pass
+
+callback=_call_zombie
+[[ $($callback) == "hey zombie" ]] ; should_pass
+
